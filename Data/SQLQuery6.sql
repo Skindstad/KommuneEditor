ce597ccc8831bf0f406c5cff3175a0eac45eede3
Skindstad <jakobskindstadskole@gmail.com>
2023-11-22 09:38:18 +0100

    All data og create class

--- Data/SQLQuery6.sql
+++ Data/SQLQuery6.sql
@@ -1,10 +1,10 @@
-Alter table Keygruppe add foreign key (Kom_nr) references Kommune;
-Alter table Keygruppe add foreign key (GruppeId) references Keysnumber;
-Alter table Keygruppe add foreign key (Aarstal) references Aarstal;
+Alter table Keysgruppe add foreign key (Kom_nr) references Kommune;
+Alter table Keysgruppe add foreign key (GruppeId) references Keysnumber;
+Alter table Keysgruppe add foreign key (Aarstal) references Aarstal;
 
 
 
 Create view GruppeRef As
-Select KeyGruppe.Kom_nr, City, Gruppe, Aarstal, tal
-From KeyGruppe Join Keysnumber on KeyGruppe.GruppeId = Keysnumber.Id
-Join Kommune on KeyGruppe.Kom_nr = Kommune.Kom_nr
+Select Keysgruppe.Kom_nr, City, Gruppe, Aarstal, tal
+From Keysgruppe Join Keysnumber on Keysgruppe.GruppeId = Keysnumber.Id
+Join Kommune on Keysgruppe.Kom_nr = Kommune.Kom_nr
